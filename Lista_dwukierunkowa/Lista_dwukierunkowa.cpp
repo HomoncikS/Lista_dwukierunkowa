0b6446b64a891cd33ec1292b3ba5d13513d7edbd
HomoncikS <seweryn.h@opoczta.pl>
2024-11-14 12:18:22 +0100

    Zaimplementowanie metod usuwania elementów oraz wyświetlania listy

--- Lista_dwukierunkowa/Lista_dwukierunkowa.cpp
+++ Lista_dwukierunkowa/Lista_dwukierunkowa.cpp
@@ -19,7 +19,6 @@ public:
     ~ListaDwukierunkowa() {
         wyczysc();
     }
-    void wyczysc() {}
 
     void dodajNaPoczatek(int wartosc) {
         Wezel* nowyWezel = new Wezel(wartosc);
@@ -44,6 +43,56 @@ public:
             koniec = nowyWezel;
         }
     }
+
+    void usunZPoczatku() {
+        if (!poczatek) return;
+        Wezel* temp = poczatek;
+        poczatek = poczatek->nastepny;
+        if (poczatek) {
+            poczatek->poprzedni = nullptr;
+        }
+        else {
+            koniec = nullptr;
+        }
+        delete temp;
+    }
+
+    void usunZKonca() {
+        if (!koniec) return;
+        Wezel* temp = koniec;
+        koniec = koniec->poprzedni;
+        if (koniec) {
+            koniec->nastepny = nullptr;
+        }
+        else {
+            poczatek = nullptr;
+        }
+        delete temp;
+    }
+
+    void wyswietl() const {
+        Wezel* obecny = poczatek;
+        while (obecny) {
+            std::cout << obecny->dane << " ";
+            obecny = obecny->nastepny;
+        }
+        std::cout << std::endl;
+    }
+
+    void wyswietlOdKonca() const {
+        Wezel* obecny = koniec;
+        while (obecny) {
+            std::cout << obecny->dane << " ";
+            obecny = obecny->poprzedni;
+        }
+        std::cout << std::endl;
+    }
+
+    void wyczysc() {
+        while (poczatek) {
+            usunZPoczatku();
+        }
+    }
 };
 
 int main() {
@@ -54,6 +103,11 @@ int main() {
         std::cout << "\nMenu:\n";
         std::cout << "1. Dodaj element na poczatek listy\n";
         std::cout << "2. Dodaj element na koniec listy\n";
+        std::cout << "3. Usun element z poczatku listy\n";
+        std::cout << "4. Usun element z konca listy\n";
+        std::cout << "5. Wyswietl cala liste\n";
+        std::cout << "6. Wyswietl liste w odwrotnej kolejnosci\n";
+        std::cout << "7. Czysc cala liste\n";
         std::cout << "0. Wyjscie\n";
         std::cout << "Wybierz opcje: ";
         std::cin >> wybor;
@@ -68,6 +122,23 @@ int main() {
             std::cout << "Podaj wartosc do dodania na koniec: ";
             std::cin >> wartosc;
             lista.dodajNaKoniec(wartosc);
+            break;
+        case 3:
+            lista.usunZPoczatku();
+            break;
+        case 4:
+            lista.usunZKonca();
+            break;
+        case 5:
+            std::cout << "Lista: ";
+            lista.wyswietl();
+            break;
+        case 6:
+            std::cout << "Lista w odwrotnej kolejnosci: ";
+            lista.wyswietlOdKonca();
+            break;
+        case 7:
+            lista.wyczysc();
             break;
         case 0:
             std::cout << "Koniec programu.\n";
